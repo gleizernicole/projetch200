f9ed76ceb2d0448702ad8ba26b5c628b187a76d9
siolene <siolene.croome@gmail.com>
2025-05-22 18:11:08 +0200

    report

--- periodictable/src/periodictable/utils.py
+++ periodictable/src/periodictable/utils.py
@@ -23,7 +23,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(current_dir, ".."))
 sys.path.insert(0, project_root)
 
-from periodictable.elements_data import elements, positions, colors, production_methods
+from periodictable.src.periodictable.elements_data import elements, positions, colors, production_methods
 
 # ======================================================================================
 # MAIN APPLICATION CLASS
